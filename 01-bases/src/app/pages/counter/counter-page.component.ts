c12abf43f8cfe1be7554deb4e7a2835907780c7d
Rignu1503 <rigoberto.miranda1503@gmail.com>
2025-09-23 18:18:28 -0500

    feat(module1): add basic routes setup and introduce signals with computed

---
+++ 01-bases/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, signal } from "@angular/core";
+
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styles: `
+    button {
+      margin-right: 5px;
+    }
+  `
+})
+export class CounterPageComponent {
+
+  counter = 10;
+  counterSignal = signal(10);
+
+  increaseBy(value: number) {
+    this.counter += value;
+    this.counterSignal.update( current => current + value );
+  }
+
+  decreaseBy(value: number) {
+    this.counter -= value;
+    this.counterSignal.update( current => current - value );
+
+  }
+  resetCounter() {
+    this.counter = 0;
+    this.counterSignal.set(0);
+
+  }
+
+}
